8ef81a2011d8ebd5e564c1c1e80c429ea255e421
Nisha Haridas <48982058+NishaHaridas@users.noreply.github.com>
2019-03-27 22:36:53 +0100

    added back navigation feature to test config

--- config/test.conf.js
+++ config/test.conf.js
@@ -4,6 +4,7 @@ const _ = require('lodash'),
 exports.config = _.merge(baseConfig.config, {
   baseUrl: 'https://www.leaseplan.com',
   specs: [
+    './features/NL/navigation.feature',
     './features/NL/client-service.feature'
   ],
   capabilities: [{
